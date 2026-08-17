6a46b2ee0bb16e4acbba64427dd216b7abc036a1
alvesrzz <gabriel.carvalho1524@gmail.com>
2026-08-17 10:43:32 -0300

    .

--- AbstractFactory/src/Main.java
+++ AbstractFactory/src/Main.java
@@ -1,19 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author aluno
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        FabricaInterface fabrica = new FabricaWindows();
+
+        Botao botao = fabrica.criarBotao();
+        CaixaSelecao caixa = fabrica.criarCaixaSelecao();
+
+        botao.exibir();
+        caixa.exibir();
     }
-    
 }
